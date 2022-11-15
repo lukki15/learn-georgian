5d908aa889060a0cd7e1200650302de1f570b005
lukki15 <50550773+lukki15@users.noreply.github.com>
2022-11-16 00:12:08 +0100

    add hard-coded character path

--- lib/src/features/writing/view/writing_screen.dart
+++ lib/src/features/writing/view/writing_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:path_drawing/path_drawing.dart';
+import 'package:drawing_animation/drawing_animation.dart';
+import 'dart:math';
 
 import 'package:learn_georgian/src/shared/extensions.dart';
 
@@ -92,7 +93,7 @@ class WritingScreen extends StatelessWidget {
                       child: SizedBox(
                           width: 46.174487,
                           height: 56.783624,
-                          child: Animation())),
+                          child: _Animated())),
                   // child: const Center(child: Text('TEST')),
                 ),
               ),
@@ -125,74 +126,48 @@ class WritingScreen extends StatelessWidget {
   }
 }
 
-class Animation extends StatefulWidget {
-  const Animation({
+class _Animated extends StatelessWidget {
+  const _Animated({
     Key? key,
   }) : super(key: key);
 
-  @override
-  State<Animation> createState() => _AnimationState();
-}
-
-class _AnimationState extends State<Animation>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(vsync: this);
-    _controller.duration = const Duration(seconds: 5);
-    _controller.value = 0.0;
-    _controller.reset();
-    _controller.forward();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    _controller.dispose();
-  }
-
-  Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: _controller,
-      builder: (context, widget) {
-        return CustomPaint(
-          painter: MyCustomPainter(progress: _controller.value),
-          child: Container(),
-        );
-      },
-    );
-  }
-}
-
-class MyCustomPainter extends CustomPainter {
-  double progress;
-
-  MyCustomPainter({required this.progress});
-
-  final Paint _paint = Paint()
+  static final Paint _paint = Paint()
     ..color = Colors.blue
     ..style = PaintingStyle.stroke
     ..strokeWidth = 2.0
     ..strokeJoin = StrokeJoin.round;
 
-  static const _aSvg =
-      "m 23.088,56.783999 q -6.448,0 -11.752,-2.392 -5.1999998,-2.392 -8.3199998,-7.072 -3.01600000926514,-4.68 -3.01600000926514,-11.648 0,-2.288 0.41599999926514,-4.368 0.52,-2.184 0.93600001,-3.432 H 10.088 q -0.4159998,1.352 -0.7279998,3.432 -0.208,1.976 -0.208,4.368 0,6.656 3.7439998,10.088 3.848,3.432 10.192,3.432 6.344,0 9.984,-3.536 3.64,-3.536 3.64,-10.504 0,-5.408 -2.288,-8.736 -2.288,-3.432 -5.304,-6.344 -2.496,-2.496 -4.992,-5.2 -2.392,-2.808 -4.056,-6.3439995 -1.664,-3.64 -1.664,-8.52800001879883 H 27.56 Q 27.56,4.4719995 29.952,7.9039995 q 2.496,3.3279995 5.72,6.5519995 2.6,2.496 4.992,5.408 2.392,2.912 3.952,6.656 1.56,3.64 1.56,8.632 0,6.448 -3.016,11.336 -3.016,4.888 -8.216,7.592 -5.2,2.704 -11.856,2.704 z";
-  final _pathMetricsForA = parseSvgPathData(_aSvg).computeMetrics();
-
   @override
-  void paint(Canvas canvas, Size size) {
-    for (final pathMetric in _pathMetricsForA) {
-      Path extractPath =
-          pathMetric.extractPath(0.0, pathMetric.length * progress);
-      canvas.drawPath(extractPath, _paint);
-    }
-  }
+  Widget build(BuildContext context) {
+    const List<Point> points = [
+      Point(1.514, 7.374),
+      Point(1.294, 8.283),
+      Point(1.210, 9.438),
+      Point(2.105, 12.314),
+      Point(3.824, 13.599),
+      Point(6.109, 14.020),
+      Point(8.569, 13.488),
+      Point(10.085, 12.190),
+      Point(10.965, 9.301),
+      Point(10.457, 7.003),
+      Point(9.232, 5.284),
+      Point(7.911, 3.880),
+      Point(6.618, 2.173),
+      Point(6.081, 0.000),
+    ];
 
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return true;
+    final path = Path();
+    path.moveTo(points[0].x.toDouble(), points[0].y.toDouble());
+    points.skip(1).forEach((element) {
+      path.lineTo(element.x.toDouble(), element.y.toDouble());
+    });
+
+    return AnimatedDrawing.paths(
+      [path],
+      paints: [_paint],
+      run: true,
+      duration: const Duration(seconds: 10),
+      onFinish: () => {},
+    );
   }
 }
